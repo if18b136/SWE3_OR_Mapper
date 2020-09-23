8f90b7927357d537a4b749b746ed6b4e35299685
if18b136 <if18b136@technikum-wien.at>
2020-09-23 02:02:18 +0200

    Warning cleanup.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,18 +1,13 @@
 import Database.DatabaseConnection;
-import Service.Config;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.util.Properties;
 
 public class Main {
     static final Logger mainLogger = LogManager.getLogger("Main Logger");
     public static void main(String[] args) {
-        try{ ;
+        try{
             DatabaseConnection db = DatabaseConnection.getInstance();
-            Connection con = db.getConnection();
+            System.out.println(db.hashCode());
         } catch (Exception e) {
             mainLogger.error(e);
         }
