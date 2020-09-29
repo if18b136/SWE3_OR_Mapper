b1f8569b33bd0ce2c69a0a4afbaa9bd59ed31cad
if18b136 <if18b136@technikum-wien.at>
2020-09-29 15:46:04 +0200

    Updated log4j2 version

--- src/main/java/Service/Config.java
+++ src/main/java/Service/Config.java
@@ -3,7 +3,6 @@ package Service;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.Properties;
