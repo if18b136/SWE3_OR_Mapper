ca3ea9e93705ba0604fe2115ba77383af9c9d615
if18b136 <if18b136@technikum-wien.at>
2020-11-17 23:58:40 +0100

    Refactored Queries

--- src/main/java/ORM/Queries/Query.java
+++ src/main/java/ORM/Queries/Query.java
@@ -3,15 +3,6 @@ package ORM.Queries;
 public interface Query {
     //TODO add table joins
 
-    char space = ' ';
-    char all = '*';
-    char eq = '=';
-    char comma = ',';
-    String from = "FROM";
-    String and = "AND";
-    String where = "WHERE";
-    char semicolon = ';';
-
     void addTargets(String... targets);
     void addTables(String... tables);
     <T> void addCondition(T column, T value);
