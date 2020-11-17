c1a5da1321f353c01809921d2717e7d0dd3a219d
if18b136 <if18b136@technikum-wien.at>
2020-11-17 15:36:58 +0100

    Added Foreign Key Annotation

---
+++ src/main/java/ORM/Annotations/ForeignKey.java
@@ -0,0 +1,14 @@
+package ORM.Annotations;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target({ElementType.FIELD})
+@Retention(RetentionPolicy.RUNTIME)
+public @interface ForeignKey {
+    String table() default "";
+    String column() default "";
+    String foreignColumn() default "";
+}
