63c86154fd9a6340321a77e239bd9c40ce27c6e0
if18b136 <if18b136@technikum-wien.at>
2020-11-22 20:52:14 +0100

    Cleanup

--- src/main/java/ORM/Manager.java
+++ src/main/java/ORM/Manager.java
@@ -6,7 +6,6 @@ import ORM.Base.Field;
 import ORM.Queries.CreateTableQuery;
 import ORM.Queries.InsertQuery;
 import ORM.Queries.SelectQuery;
-import ORM.Queries.Statement;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
@@ -17,7 +16,6 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Date;
 import java.util.ArrayList;
-import java.util.Collection;
 import java.util.HashMap;
 import java.util.List;
 
